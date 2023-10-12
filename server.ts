2d3f3f7ee9cd348b98368cd57a9b96d2ab5c4fa0
Shaqeeb Akhtar <shaqeebakhtar01@gmail.com>
2023-10-12 14:05:28 +0530

    add cors

--- server.ts
+++ server.ts
@@ -2,9 +2,19 @@ import express, { Express } from "express";
 import { router } from "./routes";
 import { PORT } from "./config";
 import { connectDB } from "./lib/db";
+import cors from "cors";
 
 const app: Express = express();
 const port = PORT || 5000;
+
+const corsOption = {
+  origin: [
+    "http://localhost:8000",
+    "http://localhost:3000",
+    "http://localhost:5000",
+  ],
+};
+app.use(cors(corsOption));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
